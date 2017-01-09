656368f748f2a25ad29470ecd4dc2a8ef4834603
Isaac Halvorson <hello@hisaac.net>
2017-01-09 15:44:20 -0600

    add boilerplate code from angular materials docs

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,4 +1,4 @@
-var app = angular.module('stementor', ['ngRoute']);
+var app = angular.module('stementor', ['ngRoute', 'ngMaterial']);
 
 app.config(['$routeProvider', function($routeProvider) {
   $routeProvider
