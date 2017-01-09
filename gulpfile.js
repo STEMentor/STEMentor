db2e449f8bb0a7ef38a5e6a78a22a8c2d74b77bb
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-09 14:20:30 -0600

    Require gulp and gulp modules

---
+++ gulpfile.js
@@ -0,0 +1,6 @@
+var gulp = require('gulp');
+var concat = require('gulp-concat');
+var cssmin = require('gulp-minify-css');
+var rename = require("gulp-rename");
+var sass = require('gulp-sass');
+var uglify = require('gulp-uglify');
