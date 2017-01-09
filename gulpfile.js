21766a7eb967375976823ec33a1de269a9f0eb79
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-09 14:51:37 -0600

    Run gulp to add css files

--- gulpfile.js
+++ gulpfile.js
@@ -4,3 +4,22 @@ var cssmin = require('gulp-minify-css');
 var rename = require("gulp-rename");
 var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
+
+gulp.task('default', ['styles', 'watch']);
+
+// styles task
+gulp.task('styles', function() {
+  return gulp.src('./src/*.scss')
+    .pipe(sass())
+    .pipe(gulp.dest('./server/public/css/'))
+    .pipe(cssmin())
+    .pipe(rename({
+      suffix: '.min'
+    }))
+    .pipe(gulp.dest('./server/public/css/'));
+});
+
+// watch task
+gulp.task('watch', function() {
+  gulp.watch('./src/*.scss', ['styles']);
+});
