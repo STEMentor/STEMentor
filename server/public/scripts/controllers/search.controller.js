0caac7c8c8635587654812a22784a13a62932c46
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-10 17:13:49 -0600

    Add get request

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,6 +1,26 @@
 app.controller('SearchController', ['$http', function($http) {
   console.log('SearchController running');
 
+  self.newSearch = {};
 
+  self.mentors = [];
+
+  self.getMentors = function() {
+    return $http({
+          method: 'GET',
+          url: '/mentors',
+          headers: {
+            newSearch: newSearch
+          }
+        })
+        .then(function (response) {
+          self.mentors = response.data;
+          console.log(mentors);
+
+        });
+    .catch(function (error) {
+      console.log('An error has occurred');
+    });
+  }
 
 }]);
