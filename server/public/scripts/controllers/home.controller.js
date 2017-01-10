e43e271d4856802c28934d163928b4c37c5854e5
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-10 14:35:30 -0600

    Add folder with image files to replace image links

--- server/public/scripts/controllers/home.controller.js
+++ server/public/scripts/controllers/home.controller.js
@@ -3,28 +3,28 @@ app.controller('HomeController', ['$http', function($http) {
     self.dataArray = [
 
       {
-        src: 'http://www.scind.org/content_images/full/1422719896Genome.jpg'
+        src: '../assets/images/carousel/genome.jpg'
       },
       {
-        src: 'http://www.technology.org/texorgwp/wp-content/uploads/2015/10/18167.jpg'
+        src: '../assets/images/carousel/falkirk-wheel.jpg'
       },
       {
-        src: 'https://s3.amazonaws.com/classconnection/580/flashcards/6473580/jpg/carcharodon_carcharias-1493631F70B6169541E.jpg'
+        src: '../assets/images/carousel/iss.jpg'
       },
       {
-        src: 'https://s-media-cache-ak0.pinimg.com/originals/4c/0b/23/4c0b2337981a1eb4aab3eb3dc0df5465.jpg'
+        src: '../assets/images/carousel/shark.jpg'
       },
       {
-        src: 'http://www.publicdomainpictures.net/pictures/180000/velka/antelope-canyon-rock-formation.jpg'
+        src: '../assets/images/carousel/snowflake.jpg'
       },
       {
-        src: 'http://circuits-central.com/wp-content/uploads/2015/09/home-2-1024x631.jpg'
+        src: '../assets/images/carousel/virus.jpg'
       },
       {
-        src: 'https://i.ytimg.com/vi/e6dS6grtt_w/maxresdefault.jpg'
+        src: '../assets/images/carousel/rock-formation.jpgg'
       },
       {
-        src: 'http://microbe.net/wp-content/uploads/2016/09/STS132_undocking_iss2.jpg'
+        src: '../assets/images/carousel/circuit-board.jpg'
       },
 
         ];
