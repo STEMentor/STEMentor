4114da4398ad38ea77c7f31dbf0b7d8c37f59a35
PhilGebauer <gebauer.phil@gmail.com>
2017-01-10 14:44:29 -0600

    add checkboxes

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -1,8 +1,7 @@
 app.controller('SearchController', ['$http', function($http) {
   console.log('SearchController running');
 
-  angular
-    .module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+  angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
     .controller('DemoCtrl', function($scope) {
       $scope.user = {
         title: 'Developer',
@@ -33,10 +32,24 @@ app.controller('SearchController', ['$http', function($http) {
 
     });
 
+  angular.module('Checkbox',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+
+    .controller('AppCtrl', function($scope) {
+
+      $scope.data = {};
+      $scope.data.cb1 = true;
+      $scope.data.cb2 = false;
+      $scope.data.cb3 = false;
+      $scope.data.cb4 = false;
+      $scope.data.cb5 = false;
+
+    });
+
+
+}]);
+
 
   /**
   Copyright 2016 Google Inc. All Rights Reserved.
   Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
   **/
-
-}]);
