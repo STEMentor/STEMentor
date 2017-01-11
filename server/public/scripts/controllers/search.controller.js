f4dc343b138ef98d1a7e1a75ef8ef137596e9cd5
PhilGebauer <gebauer.phil@gmail.com>
2017-01-11 10:17:27 -0600

    working on new inputs

--- server/public/scripts/controllers/search.controller.js
+++ server/public/scripts/controllers/search.controller.js
@@ -3,56 +3,58 @@ app.controller('SearchController', ['$http', function($http) {
 
   angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
     .controller('DemoCtrl', function($scope) {
-      $scope.user = {
-        company: 'Google',
-      };
 
-      $scope.states = (
-      'WY').split(' ').map(function(state) {
-          return {abbrev: state};
-        });
-    })
-    .config(function($mdThemingProvider) {
+      angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
+  .controller('DemoCtrl', function($scope) {
+    $scope.user = {
+      city: 'Mountain View',
 
-      // Configure a dark theme with primary foreground yellow
+    };
 
-      $mdThemingProvider.theme('docs-dark', 'default')
-        .primaryPalette('yellow')
-        .dark();
+    $scope.states = (
+    'WY').split(' ').map(function(state) {
+        return {abbrev: state};
+      });
+  })
+  .config(function($mdThemingProvider) {
+
+
+    $mdThemingProvider.theme('docs-dark', 'default')
+      .primaryPalette('yellow')
+      .dark();
+
+  });
+
+
+/**
+Copyright 2016 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
+**/
 
     });
-
-  angular.module('Checkbox',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-
-    .controller('AppCtrl', function($scope) {
-
-      $scope.data = {};
-
-
-    });
-
-  self.newSearch = {};
-
-
-  self.mentors = [];
-
-  self.getMentors = function() {
-    return $http({
-          method: 'GET',
-          url: '/mentors',
-          headers: {
-            newSearch: newSearch
-          }
-        })
-        .then(function (response) {
-          self.mentors = response.data;
-          console.log(mentors);
-
-        });
-    .catch(function (error) {
-      console.log('An error has occurred');
-    });
-  }
+  //
+  // self.newSearch = {};
+  //
+  //
+  // self.mentors = [];
+  //
+  // self.getMentors = function() {
+  //   return $http({
+  //         method: 'GET',
+  //         url: '/mentors',
+  //         headers: {
+  //           newSearch: newSearch
+  //         }
+  //       })
+  //       .then(function (response) {
+  //         self.mentors = response.data;
+  //         console.log(mentors);
+  //
+  //       });
+  //   .catch(function (error) {
+  //     console.log('An error has occurred');
+  //   });
+  // }
 
 }]);
 
