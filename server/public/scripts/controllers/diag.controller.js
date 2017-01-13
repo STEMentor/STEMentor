a6a78ef1dd07ac04a4763c35d2ffaf6cf0327854
Isaac Halvorson <hello@hisaac.net>
2017-01-13 11:08:12 -0600

    Create modal, and connect it to Log In script

---
+++ server/public/scripts/controllers/diag.controller.js
@@ -0,0 +1,30 @@
+app.controller('DiagController', ['$scope', '$mdDialog', '$firebaseAuth', function($scope, $mdDialog, $firebaseAuth){
+  console.log('diag controller is running');
+  var auth = $firebaseAuth();
+  var self = this;
+
+  var logIn = function() {
+    auth.$signInWithPopup("google").then(function(firebaseUser) {
+      console.log("Firebase Authenticated as: ", firebaseUser.user);
+    }).catch(function(error) {
+      console.log("Authentication failed: ", error);
+    });
+  };
+
+  self.hide = function() {
+    $mdDialog.hide();
+  };
+
+  self.cancel = function() {
+    $mdDialog.cancel();
+  };
+
+  self.answer = function(answer) {
+    logIn();
+    // $mdDialog.hide(answer);
+    // console.log('from dialog controller', answer);
+  };
+
+
+
+}]);
--- server/public/scripts/controllers/nav.controller.js
+++ server/public/scripts/controllers/nav.controller.js
@@ -1,28 +1,42 @@
-app.controller('NavController', ['$http', '$firebaseAuth', function($http, $firebaseAuth) {
+app.controller('NavController', ['$http', '$firebaseAuth', '$mdDialog', function($http, $firebaseAuth, $mdDialog) {
   console.log('NavController running');
   var auth = $firebaseAuth();
   var self = this;
 
   self.logIn = function() {
     auth.$signInWithPopup("google").then(function(firebaseUser) {
-      console.log("Firebase Authenticated as: ", firebaseUser.user.displayName);
+      console.log("Firebase Authenticated as: ", firebaseUser.user);
     }).catch(function(error) {
       console.log("Authentication failed: ", error);
     });
   };
 
-  // demo code from Angular Material docs to auto select the current nav item of
-  // the currently open window
-  ////
-  // (function() {
-  //   'use strict';
-  //
-  //   angular.module('MyApp',['ngMaterial', 'ngMessages', 'material.svgAssetsCache'])
-  //   .controller('AppCtrl', AppCtrl);
-  //
-  //   function AppCtrl($scope) {
-  //     $scope.currentNavItem = 'page1';
-  //   }
-  // })();
+  self.logInModal = function(ev) {
+    $mdDialog.show({
+        controller: 'DiagController as diag',
+        templateUrl: '../../views/login-modal.html',
+        targetEvent: ev,
+        clickOutsideToClose: true
+      })
+      .then(function(answer) {
+        // logIn();
+      });
+  };
 
 }]);
+
+// $scope.showAdvanced = function(ev) {
+//     $mdDialog.show({
+//       controller: DialogController,
+//       templateUrl: 'dialog1.tmpl.html',
+//       parent: angular.element(document.body),
+//       targetEvent: ev,
+//       clickOutsideToClose:true,
+//       fullscreen: $scope.customFullscreen // Only for -xs, -sm breakpoints.
+//     })
+//     .then(function(answer) {
+//       $scope.status = 'You said the information was "' + answer + '".';
+//     }, function() {
+//       $scope.status = 'You cancelled the dialog.';
+//     });
+//   };
