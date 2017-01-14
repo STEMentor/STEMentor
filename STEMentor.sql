10d837db6dff2697c2d66f228ffad9dce798a6d2
Isaac Halvorson <hello@hisaac.net>
2017-01-14 15:36:59 -0600

    Update database setup sql file and create sql documents for inserting test data

--- STEMentor.sql
+++ STEMentor.sql
@@ -1,3 +1,5 @@
+-- Create tables
+
 CREATE TABLE mentors (
   id SERIAL PRIMARY KEY,
   first_name VARCHAR(100),
@@ -17,6 +19,12 @@ CREATE TABLE mentors (
   admin BOOLEAN DEFAULT FALSE
 );
 
+CREATE TABLE students (
+	id SERIAL PRIMARY KEY,
+	name VARCHAR(100),
+	email VARCHAR(100)
+);
+
 CREATE TABLE faq (
 	id SERIAL PRIMARY KEY,
 	mentor_id INT REFERENCES mentors(id),
@@ -27,32 +35,12 @@ CREATE TABLE faq (
 CREATE TABLE messages (
 	id SERIAL PRIMARY KEY,
 	mentor_id INT REFERENCES mentors(id),
-	student_id INT REFERENCES student(id),
+	student_id INT REFERENCES students(id),
 	date_sent DATE,
 	date_replied DATE,
 	subject VARCHAR(100),
 	message VARCHAR(5000),
   reply VARCHAR(5000),
 	student_name VARCHAR(100),
-	message_read BOOLEAN
+	message_read BOOLEAN DEFAULT FALSE
 );
-
-CREATE TABLE students (
-	id SERIAL PRIMARY KEY,
-	name VARCHAR(100),
-	email VARCHAR(100)
-);
-
--- Dummy records for testing
-
-INSERT INTO mentors (first_name, last_name, email, company, job_title, zip, race, sex, orientation, birthday, school, degree, major, languages)
-VALUES ('Cormorant', 'Rogers', 'something@gmail.com', 'Potato, Inc.', 'CEO', 90210, 'Pacific-Islander', 'female', 'ambisexual', '1970-01-01', 'School of Life', 'PhD', 'Theoretical Astrology', 'Esperanto');
-
-INSERT INTO faq (mentor_id, question, answer)
-VALUES (1, 'Why is science?', 'Yes');
-
-INSERT INTO messages (mentor_id, student_id, date_sent, date_replied, subject, message, student_name, message_read)
-VALUES (1, 1, '2017-01-01', '2017-01-02', 'Science', 'So, why exactly is science?', 'Felicia', true);
-
-INSERT INTO students (name, email)
-VALUES ('Felicia', 'feliciasmith@gmail.com');
