6a571792ee1b2b21cf8abd08566b5a1af669ee35
Isaac Halvorson <hello@hisaac.net>
2017-01-14 17:43:37 -0600

    Build message get route and create message route, and begin building message read route

--- server/public/scripts/controllers/inbox.controller.js
+++ server/public/scripts/controllers/inbox.controller.js
@@ -1,3 +1,31 @@
+/*----------------------------------------------------------------------------//
+MESSAGING RULES:
+
+When a user views their inbox, all of their messages will be returned from the
+database. The front end logic will then decide how to display each message.
+Below is a guide to the logic required.
+
+For a message to appear in a student's inbox, it must:
+- Have the student's id in the 'student_id' column
+
+For a message to appear in a student's inbox, but have a 'replied' status:
+- Have text in the 'reply' column
+- Have a date in the 'date_replied' column
+
+For a message to appear in a mentor's inbox, it must:
+- Have the mentor's id in the 'mentor_id' column
+- Have text in the 'message' column
+- Have a date in the 'date_sent' column
+
+For a message to appear in a mentor's inbox, but have a 'replied' status:
+- Have text in the 'reply' column
+
+For a message to appear in a mentor's inbox, but have a 'read but not replied'
+status:
+- Not have text in the 'reply' column
+- Have 'TRUE' in the 'read' column
+//----------------------------------------------------------------------------*/
+
 app.controller('InboxController', ['$http', function($http) {
   console.log('InboxController running');
 
--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -8,26 +8,24 @@ var connectionString = require('../modules/db-config.module');
 //----------------------------------------------------------------------------//
 
 // Get messages for specific user
-router.get('/', function(req, res){
+router.get('/', function(req, res) {
   var userEmail = req.decodedToken.email;
   var userType = req.headers.type;
   var typeId = userType + '_id';
   var userDatabase = userType + 's';
   var userId = req.userId;
 
-  pg.connect(connectionString, function(error, client, done){
-    if (error) {
-      console.log('Database connection error: ', error);
-      res.sendStatus(500);
-    }
+  pg.connect(connectionString, function(error, client, done) {
+    connectionErrorCheck();
 
     // Query database to get user's ID
     // Note: This may not be necessary
     client.query(
-      'SELECT id FROM $1 WHERE email = $2',
-      [userDatabase, userEmail],
-      function (error, result) {
-        done();
+      'SELECT id FROM $1 WHERE email = $2', [userDatabase, userEmail],
+      function(error, result) {
+        done(); // Close connection to database
+        databaseErrorCheck();
+
         if (error) {
           console.log('Database SELECT error when searching for user ID: ', error);
           res.sendStatus(500);
@@ -36,12 +34,12 @@ router.get('/', function(req, res){
     )
 
     // Query messages database for all messages matching the user's ID
+    // TODO: Check if it's possible to do multiple queries in one connection
     .then(
       client.query(
-        'SELECT * FROM messages WHERE $1 = $2',
-        [typeId, userId],
-        function (error, result) {
-          done();
+        'SELECT * FROM messages WHERE $1 = $2', [typeId, userId],
+        function(error, result) {
+          done(); // Close connection to database
           if (error) {
             console.log('Database SELECT error when searching for user messages: ', error);
             res.sendStatus(500);
@@ -54,3 +52,63 @@ router.get('/', function(req, res){
 
   });
 });
+
+// Create new message when user hits the "Send" button
+router.post('/new-message', function(req, res) {
+  var userId = req.userId;
+  var userType = req.headers.type;
+  var message = req.headers.message; // TODO: transfer message info in req.headers
+
+  // Check that user is a student
+  if (userType !== 'student') {
+    console.log('User cannot create message');
+    res.sendStatus(500);
+  }
+
+  pg.connect(connectionString, function(error, client, done) {
+    connectionErrorCheck();
+
+    // Create new message record
+    client.query(
+      'INSERT INTO messages' +
+      '(mentor_id, student_id, date_sent, subject, message, student_name) ' +
+      'VALUES ($1, $2, now(), $3, $4, $5)',
+      [message.mentor_id, userId, message.subject, message.body, message.student_name],
+      function(error, result) {
+        done(); // Close connection to database
+        if (error) {
+          console.log('Error creating new message: ', error);
+          res.sendStatus(500);
+        } else {
+          res.sendStatus(201);
+        }
+      }
+    );
+  });
+
+});
+
+// Mark message as read
+router.put('/read-message', function(req, res) {
+  var userId = req.userId;
+  var messageId = req.headers.message.id; // TODO: Transfr message info in req.headers
+
+  pg.connect(connectionString, function(error, client, done) {
+    connectionErrorCheck();
+
+    client.query(
+      'UPDATE messages SET message_read = TRUE WHERE id = $1',
+      [messageId],
+      
+    );
+  });
+});
+
+function connectionErrorCheck() {
+  if (error) {
+    console.log('Database connection error: ', error);
+    res.sendStatus(500);
+  }
+}
+
+module.exports = router;
