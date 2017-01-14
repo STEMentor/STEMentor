caf78c36802edc853bbef5f1917a385954564c75
Isaac Halvorson <hello@hisaac.net>
2017-01-14 16:44:03 -0600

    Write backend logic to return all messages for specified user

--- server/routes/message.route.js
+++ server/routes/message.route.js
@@ -1,120 +1,56 @@
+//----------------------------------------------------------------------------//
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
 var connectionString = require('../modules/db-config.module');
+// TODO: Add correct decoded token variable below
+// var decodedToken = tokenDecoder;
+//----------------------------------------------------------------------------//
 
-//get messages
-router.get('/', function(req, res) {
-  console.log('req.headers: ', req.headers);
-  pg.connect(connectionString, function(error, client, done) {
+// Get messages for specific user
+router.get('/', function(req, res){
+  var userEmail = req.decodedToken.email;
+  var userType = req.headers.type;
+  var typeId = userType + '_id';
+  var userDatabase = userType + 's';
+  var userId = req.userId;
+
+  pg.connect(connectionString, function(error, client, done){
     if (error) {
-      console.log('connection erroror: ', error);
+      console.log('Database connection error: ', error);
       res.sendStatus(500);
     }
-    client.query('SELECT message FROM messages WHERE $1 = $2',
-      //[mentor_id or student_id, id value],
-      function(error, result) {
+
+    // Query database to get user's ID
+    // Note: This may not be necessary
+    client.query(
+      'SELECT id FROM $1 WHERE email = $2',
+      [userDatabase, userEmail],
+      function (error, result) {
         done();
         if (error) {
-          console.log('select query erroror: ', error);
+          console.log('Database SELECT error when searching for user ID: ', error);
           res.sendStatus(500);
         }
-        console.log('result.rows: ', result.rows);
-        res.send(result.rows);
-      });
-  });
-});
+      }
+    )
 
-
-//post new message from student
-router.post('/', function(req, res) {
-  console.log('req.body: ', req.body);
-  var newMessage = req.body;
-  pg.connect(connectionString, function(error, client, done) {
-    if (error) {
-      console.log('connection erroror: ', error);
-      res.sendStatus(500);
-    }
-    client.query('INSERT INTO messages (mentor_id, student_id, date_sent, date_replied, subject, message, student_name)' +
-      'VALUES ($1, $2, $3, $4, $5, $6, $7)',
-      //[values],
-      function(error, result) {
-        done();
-        if (error) {
-          console.log('insert query erroror: ', error);
-          res.sendStatus(500);
-        } else {
-          res.sendStatus(201);
+    // Query messages database for all messages matching the user's ID
+    .then(
+      client.query(
+        'SELECT * FROM messages WHERE $1 = $2',
+        [typeId, userId],
+        function (error, result) {
+          done();
+          if (error) {
+            console.log('Database SELECT error when searching for user messages: ', error);
+            res.sendStatus(500);
+          } else {
+            res.sendStatus(201).send(result.rows);
+          }
         }
-      });
+      )
+    );
+
   });
 });
-
-
-//mark message as"read"
-router.put('/:id', function(req, res) {
-  //something
-  pg.connect(connectionString, function(error, client, done) {
-    if (error) {
-      console.log('connection erroror: ', error);
-      res.sendStatus(500);
-    }
-    client.query('UPDATE messages SET message_read=TRUE WHERE id=$1',
-      //[id value],
-      function(error, result) {
-        if (error) {
-          console.log('update erroror: ', error);
-          res.sendStatus(500);
-        } else {
-          res.sendStatus(200);
-        }
-      });
-  });
-});
-
-
-//add reply message
-router.put('/:id', function(req, res) {
-  //something
-  pg.connect(connectionString, function(error, client, done) {
-    if (error) {
-      console.log('connection erroror: ', error);
-      res.sendStatus(500);
-    }
-    client.query('UPDATE messages SET reply=$1 WHERE id=$2', [], //change
-      function(error, result) {
-        if (error) {
-          console.log('update erroror: ', error);
-          res.sendStatus(500);
-        } else {
-          res.sendStatus(200);
-        }
-      });
-  });
-});
-
-
-//delete message
-router.delete('/:id', function(req, res) {
-  console.log('req.params: ', req.params);
-  //something
-  pg.connect(connectionString, function(error, client, done) {
-    if (error) {
-      console.log('connection erroror: ', error);
-      res.sendStatus(500);
-    }
-    client.query('DELETE FROM messages WHERE id=$1', [], //change
-      function(error, result) {
-        done();
-        if (error) {
-          console.log('delete query erroror: ', error);
-          res.sendStatus(500);
-        } else {
-          res.sendStatus(200);
-        }
-      });
-  });
-});
-
-
-module.exports = router;
