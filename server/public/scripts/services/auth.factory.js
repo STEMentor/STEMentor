fd4d5ff623959b6947aeb24f6ee13a61841fccfc
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-14 09:45:31 -0600

    Add get request to logIn

--- server/public/scripts/services/auth.factory.js
+++ server/public/scripts/services/auth.factory.js
@@ -12,50 +12,68 @@ app.factory('AuthFactory', ['$http', '$firebaseAuth', function($http, $firebaseA
       console.log('firebaseUser.user.email: ', firebaseUser.user.email);
       currentUser = firebaseUser.user;
       console.log('currentUser: ', currentUser);
-      loggedIn = true;
-      // Adding new user to db
-      $http.get('/login')
-      .then(function(response) {
-        console.log('login response.data: ', response.data);
-        users = response.data;
-      }).then(function() {
-        for (var i = 0; i < users.length; i++) {
-          if(currentUser.email == users[i].email) {
-            emailInDatabase = true;
-          }
-        }
-        if (emailInDatabase === false) {
-          console.log('trying to add currentUser: ', currentUser);
-          $http.post('/login', currentUser)
-          .then(function(response) {
-            console.log('added user to db:');
+      console.log('firebaseUser: ', firebaseUser);
+      var type = 'mentor';
+      if(currentUser) {
+        currentUser.getToken().then(function(idToken){
+          console.log('ID TOKEN:', idToken)
+          $http({
+            method: 'GET',
+            url: '/users.route' + type,
+            headers: {
+              id_token: idToken
+            }
           });
-        }
+          // loggedIn = true;
+
+        });
+      }
+    });
+  }
+    // Adding new user to db
+    //   $http.get('/login')
+    //   .then(function(response) {
+    //     console.log('login response.data: ', response.data);
+    //     users = response.data;
+    //   }).then(function() {
+    //     for (var i = 0; i < users.length; i++) {
+    //       if(currentUser.email == users[i].email) {
+    //         emailInDatabase = true;
+    //       }
+    //     }
+    //     if (emailInDatabase === false) {
+    //       console.log('trying to add currentUser: ', currentUser);
+    //       $http.post('/login', currentUser)
+    //       .then(function(response) {
+    //         console.log('added user to db:');
+    //       });
+    //     }
+    //   });
+    // }).catch(function(error) {
+    //   console.log("Authentication failed: ", error);
+    // });
+
+
+    function logOut() {
+      return auth.$signOut().then(function() {
+        currentUser = undefined;
+        loggedIn = false;
+        console.log('logged out');
+        console.log('currentUser: ', currentUser);
       });
-    }).catch(function(error) {
-      console.log("Authentication failed: ", error);
-    });
-  }
-
-  function logOut() {
-    return auth.$signOut().then(function() {
-      currentUser = undefined;
-      loggedIn = false;
-      console.log('logged out');
-      console.log('currentUser: ', currentUser);
-    });
-  }
-
-  var publicApi = {
-    logIn: function() {
-      return logIn();
-    },
-    logOut: function() {
-      return logOut();
     }
-  };
 
-  return publicApi;
+    var publicApi = {
+      auth: auth,
+      logIn: function() {
+        return logIn();
+      },
+      logOut: function() {
+        return logOut();
+      }
+    };
+
+    return publicApi;
 
 
-}]);
+  }]);
