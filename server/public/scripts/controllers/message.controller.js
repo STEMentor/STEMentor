6b4a03d41946dcdb7fe2ec76f60180c6e9f6008f
Andrew Froiland <andrew.froiland@gmail.com>
2017-01-15 14:17:57 -0600

    Message-modal sending newMessage to message.controller

--- server/public/scripts/controllers/message.controller.js
+++ server/public/scripts/controllers/message.controller.js
@@ -1,6 +1,11 @@
 app.controller('MessageController', ['$http', function($http) {
   console.log('MessageController running');
+  var self = this;
+  self.newMessage = [];
 
-
+  self.sendMessage = function(){
+    console.log('newMessage: ', self.newMessage);
+    
+  }
 
 }]);
