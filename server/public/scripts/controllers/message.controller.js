657be31b126714b4631f8da56d1c59450da98d1d
Andrew Froiland <andrew.froiland@gmail.com>
2017-01-15 14:27:15 -0600

    add notes

--- server/public/scripts/controllers/message.controller.js
+++ server/public/scripts/controllers/message.controller.js
@@ -6,6 +6,20 @@ app.controller('MessageController', ['$http', function($http) {
   self.sendMessage = function(){
     console.log('newMessage: ', self.newMessage);
     
-  }
+  };
 
 }]);
+
+
+
+
+// To create a new message when a student clicks "Send Message"
+// 	- Send a `POST` request to `/message/new-message`
+// 		- Must send token to be decoded
+// 		- Must send a `message` object on data following this template:
+// 			message {
+// 				mentorEmail: 'mentor's email address',
+// 				subject: 'subject text',
+// 				body: 'body text',
+// 				studentName: 'optional student name'
+// 			}
