a8aea73db45fd21786777482222ece12ca56ed56
Oliver Alexander <oliveralexander00@gmail.com>
2017-01-17 16:27:22 -0600

    Add functions to BioFactory

--- server/public/scripts/services/bio.factory.js
+++ server/public/scripts/services/bio.factory.js
@@ -2,12 +2,51 @@ app.factory('BioFactory', ['$http', function($http){
   console.log('BioFactory running');
 
   var mentorInfo = {};
+  var mentors = {};
+  var mentor = {};
 
+
+  function getMentors(newSearch){
+    console.log("NEW SEARCH:", newSearch);
+    var newSearchString = JSON.stringify(newSearch);
+    return $http({
+        method: 'GET',
+        url: '/mentor-search/search',
+        headers: {
+          newSearchString: newSearchString
+        }
+      })
+      .then(function(response) {
+
+        console.log("--------------------------------------");
+        mentors.info = response.data;
+        console.log("Mentors list:", mentors);
+
+
+      }),
+      function(err) {
+        console.log("Error with search get request ", err);
+      };
+  };
+
+  function setMentorId(mentorId){
+    mentor.id = mentorId;
+  }
 
 
 
   var publicApi = {
-    
+    mentors: mentors,
+    mentor: mentor,
+    getMentors: function(newSearch){
+      return getMentors(newSearch);
+    },
+    setMentorId: function(mentorId){
+      return setMentorId(mentorId);
+    },
+    // getMentorId: function(){
+    //   return getMentorId();
+    // }
   };
 
   return publicApi;
