6d6286f70479ac833043dc2ddf0444befcf5bb3b
PhilGebauer <gebauer.phil@gmail.com>
2017-01-18 08:19:17 -0600

    added faq stuff

--- server/public/scripts/controllers/profile.controller.js
+++ server/public/scripts/controllers/profile.controller.js
@@ -1,5 +1,6 @@
-app.controller('ProfileController', ['$http', '$mdDialog', function($http, $mdDialog) {
+app.controller('ProfileController', ['$http', '$mdDialog', function($http, $mdDialog, $scope) {
   console.log('ProfileController running');
+
   var self = this;
 
   self.imagePath = '../server/public/assets/images/cooldog.jpeg';
@@ -7,6 +8,9 @@ app.controller('ProfileController', ['$http', '$mdDialog', function($http, $mdDi
   self.fields = {
     bio: ""
   };
+
+
+
 
   self.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
 'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
@@ -24,10 +28,17 @@ app.controller('ProfileController', ['$http', '$mdDialog', function($http, $mdDi
     });
   };
 
-
-  /**
-  Copyright 2016 Google Inc. All Rights Reserved.
-  Use of this source code is governed by an MIT-style license that can be foundin the LICENSE file at http://material.angularjs.org/HEAD/license.
-  **/
+//   $scope.test = 'Test Message';
+// }).directive("ngPortlet", function ($compile) {
+// return {
+// template: '<div>{{test}}</div>   ',
+// restrict: 'E',
+// link: function (scope, elm) {
+//     scope.add = function(){
+//         console.log(elm);
+//        elm.after($compile('<ng-portlet></ng-portlet>')(scope));
+//     }
+// }
+// };
 
 }]);
