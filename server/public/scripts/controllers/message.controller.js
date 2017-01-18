a7ed5099c19693d4c300d2995f8f55a66812f4c9
Andrew Froiland <andrew.froiland@gmail.com>
2017-01-18 10:33:35 -0600

    Build sendMessage function in message controller

--- server/public/scripts/controllers/message.controller.js
+++ server/public/scripts/controllers/message.controller.js
@@ -4,7 +4,7 @@ app.controller('MessageController', ['$http', 'AuthFactory', '$mdDialog', 'BioFa
   self.newMessage = [];
   var authInfo = AuthFactory.auth;
   var userStatus = AuthFactory.userStatus;
-  // var messageInfo =
+  var messageInfo = {};
 
   // console.log(InboxController.selectedMessage);
 
@@ -12,28 +12,55 @@ app.controller('MessageController', ['$http', 'AuthFactory', '$mdDialog', 'BioFa
 
   self.mentor = BioFactory.mentorBio.info;
 
-  self.sendMessage = function(message){
-    console.log('newMessage: ', self.newMessage);
-    console.log('authInfo: ', authInfo);
-    console.log('userStatus: ', userStatus);
-    console.log('self.mentor: ', self.mentor);
-    // $http.post('/message/new-message', self.newMessage);
-    //   .then(function(response) {
-    //     console.log('response: ', response);
-    //   });
-  // };
-
-  };
-
   self.cancel = function() {
     $mdDialog.cancel();
   };
 
 
+  self.testMessage = function(message){
+    console.log('newMessage: ', self.newMessage);
+    console.log('authInfo: ', authInfo);
+    console.log('userStatus: ', userStatus);
+    console.log('self.mentor: ', self.mentor);
+    // Makes sure that currentUser is set before getting messages from the server
+    AuthFactory.auth.$onAuthStateChanged(function(currentUser) {
+      sendMessage(currentUser);
+    });
+  };
+
+
+
+  // User creating a new message
+  function sendMessage(currentUser, message) {
+    if(currentUser){
+      return currentUser.getToken().then(function(idToken) {
+        messageInfo = self.mentor;
+        messageInfo.msgName = self.newMessage.name;
+        messageInfo.msgSubject = self.newMessage.subject;
+        messageInfo.msgBody = self.newMessage.body;
+        console.log('messageInfo: ', messageInfo);
+        return $http({
+          method: 'POST',
+          url: '/message/new-message',
+          headers: {
+            id_token: idToken
+          },
+          data: {
+            message_info: messageInfo
+          }
+        })
+        .then(function(response) {
+          self.messages = response.data;
+          console.log('Adding new message, messageInfo: ', messageInfo);
+        }),
+        function(error) {
+          console.log('Error with messages POST request: ', error);
+        };
+      });
+    }
+  }
 
 }]);
-
-
 
 
 // To create a new message when a student clicks "Send Message"
