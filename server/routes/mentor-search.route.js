c5b19bdb70ad458505d7e9a1d63af26a90f5e835
Isaac Halvorson <hello@hisaac.net>
2017-01-20 16:53:01 -0600

    Refactor query builder function to user PG bling values

--- server/routes/mentor-search.route.js
+++ server/routes/mentor-search.route.js
@@ -2,6 +2,7 @@
 var express = require('express');
 var router = express.Router();
 var pg = require('pg');
+var pg_escape = require('pg-escape');
 var connectionString = require('../modules/db-config.module');
 //----------------------------------------------------------------------------//
 
@@ -32,15 +33,16 @@ router.get('/profile/:id', function(req, res) {
     });
   });
 });
+//----------------------------------------------------------------------------//
 
 // For searching the database from the search page ---------------------------//
 router.get('/search', function(req, res) {
 
   var queryObject = JSON.parse(req.headers.newsearchstring);
-  // console.log('Query object: ', queryObject);
+  console.log('Query object from client: ', queryObject);
 
   var query = queryBuilder(queryObject);
-  // console.log('Built query: ', query);
+  console.log('Built query: ', query);
 
   pg.connect(connectionString, function(error, client, done) {
     if (error) {
@@ -60,48 +62,89 @@ router.get('/search', function(req, res) {
       res.send(result.rows);
     });
   });
+
 });
+//----------------------------------------------------------------------------//
 
-// Cunstructs SQL query based off of user defined search paramaters ----------//
+// Constructs SQL query based off of user defined search paramaters ----------//
 function queryBuilder(object) {
-
+  var index = 0;
   var query;
 
-  if(object.generic_search) {
+  // If the generic search bar was used, the `object.generic_search` property
+  // will have a value, and we need to build a query based on that.
+  if (object.generic_search) {
+    index++;
     query = 'SELECT * FROM mentors ' +
-            'WHERE first_name ILIKE \'%'    + object.generic_search +
-            '%\' OR last_name ILIKE \'%'    + object.generic_search +
-            '%\' OR email ILIKE \'%'        + object.generic_search +
-            '%\' OR blurb ILIKE \'%'        + object.generic_search +
-            '%\' OR bio ILIKE \'%'          + object.generic_search +
-            '%\' OR company ILIKE \'%'      + object.generic_search +
-            '%\' OR job_title ILIKE \'%'    + object.generic_search +
-            '%\' OR race ILIKE \'%'         + object.generic_search +
-            '%\' OR gender ILIKE \'%'       + object.generic_search +
-            '%\' OR orientation ILIKE \'%'  + object.generic_search +
-            '%\' OR school ILIKE \'%'       + object.generic_search +
-            '%\' OR degree ILIKE \'%'       + object.generic_search +
-            '%\' OR major ILIKE \'%'        + object.generic_search +
-            '%\' OR languages ILIKE \'%'    + object.generic_search +
-            '%\' AND ';
+            'WHERE first_name ILIKE %$1% ' +
+            'WHERE last_name ILIKE %$1% ' +
+            'WHERE blurb ILIKE %$1% ' +
+            'WHERE bio ILIKE %$1% ' +
+            'WHERE company ILIKE %$1% ' +
+            'WHERE job_title ILIKE %$1% ' +
+            'WHERE race ILIKE %$1% ' +
+            'WHERE gender ILIKE $1 ' +
+            'WHERE orientation ILIKE %$1% ' +
+            'WHERE school ILIKE %$1% ' +
+            'WHERE degree ILIKE %$1% ' +
+            'WHERE major ILIKE %$1% ' +
+            'WHERE languages ILIKE %$1% ' +
+            'AND ';
+
+    // This loops through the properties of the query object
+    for (var property in object) {
+      if (object[property] && property !== 'generic_search') {
+
+        // If gender has been specified in the search, we need to match this
+        // exactly, not loosely like the other queries.
+        if (property === 'gender') {
+          index++;
+          query += property + ' = $' + index + ' AND ';
+        }
+
+        else {
+          index++;
+          query += property + ' ILIKE $' + index + ' AND ';
+        }
+      }
+    }
+
+  // If no generic search was specified, we set the standard query string, and
+  // loop through the same way as above.
   } else {
     query = 'SELECT * FROM mentors WHERE ';
-  }
 
-  for (var property in object) {
-    if (object[property] && property !== 'generic_search') {
-      query += property + ' ILIKE \'' + object[property] + '\' AND ';
+    for (var property in object) {
+      if (object[property] && property !== 'generic_search') {
+        if (property === 'gender') {
+          index++;
+          query += property + ' = $' + index + ' AND ';
+        } else {
+          index++;
+          query += property + ' ILIKE $' + index + ' AND ';
+        }
+      }
     }
   }
 
+  // Slice off the last four characters to remove the trailing 'AND'
   query = query.slice(0, -4);
-  if (query == 'SELECT * FROM mentors W') {
+
+  // If no search filters were specified, the slice will still remove the last
+  // four characters. The query well end up being 'SELECT * FROM mentors WH',
+  // so this will set it to be a search to return all mentors from the database.
+  if (query == 'SELECT * FROM mentors WH') {
     query = 'SELECT * FROM mentors';
   }
 
-  console.log('Query after query builder: ', query);
-
   return query;
 }
+//----------------------------------------------------------------------------//
+
+// Constructs an array of property names for PG to use in the query
+function propertyArrayBuilder(object) {
+  
+}
+//----------------------------------------------------------------------------//
 
 module.exports = router;
