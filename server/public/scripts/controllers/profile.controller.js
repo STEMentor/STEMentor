9a9b749c233f8b31f1b4c088b4964c2daee2a6d9
Isaac Halvorson <hello@hisaac.net>
2017-01-21 16:14:29 -0600

    Create needed models in profile.controller and fix syntax errors in profile.html

--- server/public/scripts/controllers/profile.controller.js
+++ server/public/scripts/controllers/profile.controller.js
@@ -8,7 +8,6 @@ app.controller('ProfileController', ['$http', '$mdDialog', 'BioFactory', 'AuthFa
 
   self.userStatus = AuthFactory.userStatus;
 
-  // Not currently used
   self.states = (
     'AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
     'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV ' +
@@ -18,6 +17,8 @@ app.controller('ProfileController', ['$http', '$mdDialog', 'BioFactory', 'AuthFa
       abbrev: state
     };
   });
+
+  self.primaryStemField = '';
 
   // This is an object containing all of mentor's fields
   self.mentor = BioFactory.mentorBio;
