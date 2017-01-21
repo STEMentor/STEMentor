b808ea90b513e0ffc83843e74359aa6dc5f41243
Andrew Froiland <andrew.froiland@gmail.com>
2017-01-21 14:29:20 -0600

    Add delayed popup when mentor logs in for the first time

--- server/public/scripts/controllers/login.controller.js
+++ server/public/scripts/controllers/login.controller.js
@@ -18,8 +18,16 @@ app.controller('LoginController', ['$scope', '$mdDialog', '$firebaseAuth', 'Auth
         console.log('duh');
         self.cancel();
       }
+      setTimeout(firstTimeMentor, 1000);
     });
   };
+
+  function firstTimeMentor() {
+    console.log('and here, AuthFactory.userStatus.newUser: ', AuthFactory.userStatus);
+    if (AuthFactory.userStatus.newUser === true && AuthFactory.userStatus.userType == 'mentor') {
+      alert('fill out yer profile');
+    }
+  }
 
   self.logOut = function() {
     console.log('logging user out');
